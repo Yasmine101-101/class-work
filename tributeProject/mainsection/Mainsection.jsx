e9eca0d5e410c3607764b2a151f57853928df496
Yasmin101-101 <yasminmohamudyasmin@gmail.com>
2026-01-16 12:10:27 +0300

    a comment was deleted in file mainsection.jsx

--- tributeProject/mainsection/Mainsection.jsx
+++ tributeProject/mainsection/Mainsection.jsx
@@ -1,15 +1,4 @@
-// import Imagesection from "./Imagesection";
-// import Infosection from "./info/Infosection";
 
-// function Mainsection() {
-//   return (
-//     <div>
-//         <Imagesection />
-//         <Infosection />
-//     </div>
-//   );
-// }
-// export default Mainsection;
 
 import Imagesection from "./Imagesection";
 import Infosection from "./info/info.jsx";
